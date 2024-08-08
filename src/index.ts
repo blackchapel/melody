ed40fec9188f660772a89304f5afc78c700a70b8
blackchapel <kc.dark@protonmail.com>
2024-08-08 13:15:56 +0530

    Test API endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import cors from 'cors';
 import { config } from 'dotenv';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import DiscordClient from './utils/init';
+import path from 'path';
+import { createReadStream } from 'fs';
+import serveFavicon from 'serve-favicon';
 
 // Initailizing env variables
 config();
@@ -23,9 +26,17 @@ const PORT: number | undefined = 3000 || process.env.PORT;
 // Formatting incoming data and allowing cross origin requests
 app.use(cors());
 app.use(express.json());
+app.use(serveFavicon(path.join('public', 'favicon.ico')));
 
 // Logging
 app.use(morgan('dev'));
+
+// Test API endpoint
+app.use('/api', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: `Server running on ${process.env.API_URL}`
+    });
+});
 
 // Listening on the port
 app.listen(PORT, () => {
